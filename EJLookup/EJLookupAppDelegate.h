544074f805b16f09936bc0a59ef316742f463bf3
Ivan Podogov <ginkage@yuzuki.local>
2011-10-24 21:49:39 +0400

    Initial Commit

---
+++ EJLookup/EJLookupAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  EJLookupAppDelegate.h
+//  EJLookup
+//
+//  Created by Ivan Podogov on 24.10.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EJLookupAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ EJLookup/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  EJLookup
+//
+//  Created by Ivan Podogov on 24.10.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
